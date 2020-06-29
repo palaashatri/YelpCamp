88a5c368dca5d3c65a35159beb33163abf9c4c53
Palaash Atri <palaash.atri@gmail.com>
2020-06-29 21:50:03 +0530

    v5 init, add basic styling to show campgrounds page

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ mongoose.set('useUnifiedTopology', true);
 mongoose.connect("mongodb://localhost/yelp_camp",{useNewUrlParser:true});
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
+app.use(express.static(__dirname+"/public"));
 
 seedDB(); //seed the database with starter data
 
