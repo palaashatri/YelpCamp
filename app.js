4b798869035da8e8258e0513dd4b71dfaa1989b6
Palaash Atri <palaash.atri@gmail.com>
2020-07-03 19:30:06 +0530

    added user registeration functionality

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const { serializeUser } = require('passport');
+
 var express = require('express'),
     app = express(),
     bodyParser = require("body-parser"),
@@ -16,6 +18,18 @@ app.set("view engine","ejs");
 app.use(express.static(__dirname+"/public"));
 
 seedDB(); //seed the database with starter data
+
+//PASPPORT CONFIG
+app.use(require("express-session")({
+    secret : "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized : false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req,res){
     res.render("landing");
@@ -110,6 +124,29 @@ app.post("/campgrounds/:id/comments",function(req,res){
     });
 
    
+});
+
+//=========
+// AUTH ROUTES
+//=========
+
+//show register form
+app.get("/register",function(req,res){
+    res.render("register");
+});
+
+//handle signup logic
+app.post("/register",function(req,res){
+    var newUser = new User({username:req.body.username});
+    User.register(newUser,req.body.password,function(err,user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req,res,function(){
+            res.redirect("/campgrounds");
+        });
+    })
 });
 
 app.listen(5000, function () {
