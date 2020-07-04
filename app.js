59ee9905127f026e1809153b27b16197ec711202
Palaash Atri <palaash.atri@gmail.com>
2020-07-05 02:08:10 +0530

    added Login functionality

--- app.js
+++ app.js
@@ -149,6 +149,18 @@ app.post("/register",function(req,res){
     })
 });
 
+//show login form
+app.get("/login",function(req,res){
+    res.render("login");
+});
+
+//handle login logic
+app.post("/login",passport.authenticate("local",
+    {
+    successRedirect:"/campgrounds",
+    failureRedirect:"/login"
+    }),function(req,res){});
+
 app.listen(5000, function () {
     console.log("Server has started");
 });
