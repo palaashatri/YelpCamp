910ec504c46ad43cca113a37ce7918919d80bb85
Palaash Atri <palaash.atri@gmail.com>
2020-07-05 23:07:47 +0530

    Show/hide links in navbar correctly!

--- app.js
+++ app.js
@@ -31,6 +31,15 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// middleware to send currentUser to header.ejs
+app.use(function(req,res,next){
+    res.locals.currentUser = req.user;
+    next();
+});
+
+
+// ROUTES ====================
+
 app.get("/", function(req,res){
     res.render("landing");
 });
