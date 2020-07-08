ac1bb4c8d5347178cf08eed1cdede329a3ff0e17
Palaash Atri <palaash.atri@gmail.com>
2020-07-08 21:54:59 +0530

    🐱‍🚀 User + Campgrounds association! - isLoggedIn middleware to both "create" and "new" routes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -16,12 +16,16 @@ router.get("/",function(req,res){
 });
 
 //CREATE ROUTE : add a new campground to DB
-router.post("/",function (req,res) {
+router.post("/",isLoggedIn,function (req,res) {
    //get data from form and add to campgrounds array
    var name = req.body.name;
    var image = req.body.image;
    var desc = req.body.description;
-   var newCampground = {name : name, image : image, description : desc};
+   var author = {
+       id:req.user._id,
+       username:req.user.username
+   };
+   var newCampground = {name : name, image : image, description : desc,author:author};
    //create a new CG and save to DB
    Campground.create(newCampground,function(err,newlyCreated){
         if(err){
@@ -35,7 +39,7 @@ router.post("/",function (req,res) {
 });
 
 //NEW :  Displays form to add a new CG to DB
-router.get("/new",function(req,res){
+router.get("/new",isLoggedIn,function(req,res){
     res.render("campgrounds/new");
 });
 
@@ -51,5 +55,13 @@ router.get("/:id",function(req,res){
         }
     });
 });
+
+//middleware
+function isLoggedIn(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 module.exports = router;
