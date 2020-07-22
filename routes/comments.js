58c7b45c1eae0870766b04cb525b43b85ce910a0
Palaash Atri <palaash.atri@gmail.com>
2020-07-23 01:29:50 +0530

    🔧Comments Authorization implemented!

--- routes/comments.js
+++ routes/comments.js
@@ -45,7 +45,7 @@ router.post("/",isLoggedIn,function(req,res){
 });
 
 // COMMENT EDIT ROUTE
-router.get("/:comment_id/edit",function(req,res){
+router.get("/:comment_id/edit",checkCommentOwnership,function(req,res){
     Comment.findById(req.params.comment_id,function(err,foundComment){
         if(err){
             res.redirect("back");
@@ -57,7 +57,7 @@ router.get("/:comment_id/edit",function(req,res){
 });
 
 // COMMENT UPDATE
-router.put("/:comment_id", function(req,res){
+router.put("/:comment_id", checkCommentOwnership,function(req,res){
     Comment.findByIdAndUpdate(req.params.comment_id,req.body.comment,function(err,updatedComment){
         if(err){
             res.redirect("back");
@@ -68,7 +68,7 @@ router.put("/:comment_id", function(req,res){
 });
 
 // COMMENTS DESTROY ROUTE
-router.delete("/:comment_id",function(req,res){
+router.delete("/:comment_id",checkCommentOwnership,function(req,res){
     Comment.findByIdAndRemove(req.body.params.comment_id,function(err){
         if(err){
             res.redirect("back");
@@ -79,6 +79,27 @@ router.delete("/:comment_id",function(req,res){
 });
 
 //middleware
+
+function checkCommentOwnership(req,res,next){
+    if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id,function(err,foundComment){
+            if(err){
+                res.redirect("back");
+            } else{
+                // does user own the comment?
+                if(foundComment.author.id.equals(req.user._id)){
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+                
+            }
+        });        
+    } else {
+        res.redirect("back");
+    }    
+}
+
 function isLoggedIn(req,res,next){
     if(req.isAuthenticated()){
         return next();
